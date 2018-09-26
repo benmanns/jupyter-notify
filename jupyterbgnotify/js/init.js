fd7b3241deb59c6613e1b615e7968b3c6b3a4c58
Benjamin Manns <benmanns@gmail.com>
2018-09-26 19:15:35 -0400

    Update cell magic in JS alert

--- jupyterbgnotify/js/init.js
+++ jupyterbgnotify/js/init.js
@@ -1,5 +1,5 @@
 if (!("Notification" in window)) {
-    alert("This browser does not support desktop notifications, so the %%bgnotify magic will not work.");
+    alert("This browser does not support desktop notifications, so the %%notify magic will not work.");
 } else if (Notification.permission !== 'granted' && Notification.permission !== 'denied') {
     Notification.requestPermission(function (permission) {
         if(!('permission' in Notification)) {
